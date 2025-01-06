7bdd79c257b7429d8fa5aeeb84cb03e933bded94
varun kamani <varunphp.dev@gmail.com>
2025-01-06 18:50:48 +0530

    Removed unused CSS and index files to clean up the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import Router from "./routes";
+import { CssBaseline } from "@mui/material";
+import Layout from "./componenets/Layout";
+// import Router from "./routes";
+import { Route, Routes } from "react-router-dom";
+import ProjectList from "./pages/project/ProjectList";
+import ProjectDetail from "./pages/project/ProjectDetail";
+import ProjectProvider from "./context/ProjectProvider";
 
 function App() {
   return (
     <>
-      <Router />
+      <ProjectProvider>
+        <CssBaseline />
+        <Layout>
+          <Routes>
+            <Route path="/" element={<ProjectList />} />
+            <Route path="/project/add" element={<ProjectDetail />} />
+            <Route path="/project/:id" element={<ProjectDetail />} />
+          </Routes>
+        </Layout>
+      </ProjectProvider>
     </>
   );
 }
